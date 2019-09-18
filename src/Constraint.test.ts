47a58580757298b412ed75b04e791d8ff8b1e59d
hiroto7 <hiroto450@gmail.com>
2019-09-18 15:45:29 +0900

    Constraint.getChildByProperty()に関するテストを追加

--- src/Constraint.test.ts
+++ src/Constraint.test.ts
@@ -2,7 +2,7 @@ import Constraint, { $array, $boolean, $const, $false, $never, $null, $number, $
 import JSONTypeError from "./JSONTypeError";
 
 describe('Constraint', () => {
-  describe('Constraint.typeName()', () => {
+  describe('Constraint.typeName', () => {
     test('boolean', () => {
       expect($boolean.typeName).toBe('boolean');
     });
@@ -76,6 +76,57 @@ describe('Constraint', () => {
         a: $number,
         b: $union($const(true), $string)
       }).typeName).toBe('{ "a": number; "b": true | string; }')
+    });
+  });
+
+  describe('Constraint.getChildByProperty()', () => {
+    describe(`プリミティブ型の子プロパティは 'never' 型である`, () => {
+      const table: [string, Constraint][] = [
+        $string, $const('fuga'), $number, $const(2), $boolean, $true, $false, $null, $undefined,
+        $union($number, $boolean),
+        $union($null, $undefined)
+      ].map(constraint => [constraint.typeName, constraint]);
+
+      test.each(table)(
+        `型 '%s' の子プロパティは 'never' 型である`,
+        (_, constraint) => {
+          expect(constraint.getChildByProperty('a')).toBe($never);
+        });
+    });
+
+    describe('オブジェクト型に定義された子プロパティの型', () => {
+      const table0: [Constraint, Constraint][] = [
+        [$object({ a: $string }), $string],
+        [$object({ a: $union($number, $boolean) }), $union($number, $boolean)],
+        [$object({ a: $object({ b: $string }) }), $object({ b: $string })],
+        [$union($object({ a: $number }), $object({ a: $boolean })), $union($number, $boolean)],
+        [$union($object({ a: $null }), $object({ a: $undefined }), $string), $union($null, $undefined)]
+      ];
+      const table: [string, string, Constraint][] = table0.map(([constraint, childConstraint]: [Constraint, Constraint]) => [constraint.typeName, childConstraint.typeName, constraint]);
+
+      test.each(table)(
+        `型 '%s' のプロパティ 'a' は '%s' 型である`,
+        (_, childTypeName, constraint) => {
+          expect(constraint.getChildByProperty('a')).not.toBeNull();
+          expect(constraint.getChildByProperty('a')).not.toBeUndefined();
+          expect(constraint.getChildByProperty('a')!.typeName).toBe(childTypeName);
+        }
+      );
+    });
+
+    describe(`オブジェクト型に定義されていない子プロパティに関する制約は存在しない`, () => {
+      const table: [string, Constraint][] = [
+        $object({}),
+        $union($object({ a: $number }), $object({ b: $boolean })),
+        $union($object({ a: $null }), $object({ b: $undefined }), $string)
+      ].map(constraint => [constraint.typeName, constraint]);
+
+      test.each(table)(
+        `型 '%s' のプロパティ 'a' に関する制約は存在しない`,
+        (_, constraint) => {
+          expect(constraint.getChildByProperty('a')).toBeNull();
+        }
+      );
     });
   });
 

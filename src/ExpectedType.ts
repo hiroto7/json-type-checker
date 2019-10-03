e85b75f51afceae085c6bd7eff80b7d361fc986c
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2019-10-04 07:34:34 +0900

    Update ExpectedType<ObjectConstraint<O>>

--- src/ExpectedType.ts
+++ src/ExpectedType.ts
@@ -1,4 +1,4 @@
-import Constraint, { NumberConstraint, StringConstraint, BooleanConstraint, ArrayConstraint, UnionConstraint, ObjectConstraint, ConstantConstraint, NeverConstraint } from "./Constraint";
+import Constraint, { ArrayConstraint, BooleanConstraint, ConstantConstraint, NeverConstraint, NumberConstraint, ObjectConstraint, OptionalConstraint, StringConstraint, UnionConstraint } from "./Constraint";
 
 type ExpectedType<C extends Constraint> =
   C extends NumberConstraint ? number :
@@ -8,9 +8,16 @@ type ExpectedType<C extends Constraint> =
   C extends UnionConstraint<readonly (infer CS & Constraint)[]> ? (
     CS extends Constraint ? { 0: ExpectedType<CS> }[C extends C ? 0 : never] :
     never) :
-  C extends ObjectConstraint<infer D> ? { [P in keyof D]: ExpectedType<D[P]> } :
+  C extends ObjectConstraint<infer O> ? (
+    { [P in RequiredKeys<O>]: ExpectedType<O[P]> } &
+    { [P in OptionalKeys<O>]?: ExpectedType<O[P]> }) :
   C extends ConstantConstraint<infer D> ? D :
   C extends NeverConstraint ? never :
   unknown;
+
+type RequiredKeys<O extends { [P in keyof O]: Constraint }> = Exclude<keyof O, OptionalKeys<O>>;
+type OptionalKeys<O extends { [P in keyof O]: Constraint }> = {
+  [P in keyof O]: O[P] extends OptionalConstraint<Constraint[]> ? P : never;
+}[keyof O];
 
 export default ExpectedType;
